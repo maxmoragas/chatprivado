e2247bb2502599a8493006b10ccdf25e973162b2
maxmoragas <ixamxp@gmail.com>
2025-05-07 21:55:02 -0300

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,6 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, updateProfile } from "firebase/auth";
-import { getFirestore, doc, setDoc, getDoc } from "firebase/firestore";
+import { getFirestore, doc, setDoc } from "firebase/firestore";
 
 const firebaseConfig = {
     apiKey: "AIzaSyCalxt34jrPFP9VJM5yBFA4BRF2U1_XiZw",
@@ -8,23 +8,19 @@ const firebaseConfig = {
     projectId: "michatprivado-f704a",
     storageBucket: "michatprivado-f704a.appspot.com",
     messagingSenderId: "187774286181",
-    appId: "1:187774286181:web:95fc9391a64d3d244e498c" // ID de la app corregido
+    appId: "1:187774286181:web:95fc9391a64d3d244e498c"
 };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 
-// Función para registrar usuario con email, contraseña y nickname
-async function registerUser(email, password, nickname) {
+export async function registerUser(email, password, nickname) {
     try {
         const userCredential = await createUserWithEmailAndPassword(auth, email, password);
         const user = userCredential.user;
 
-        // Guarda el nickname en Firebase Authentication
         await updateProfile(user, { displayName: nickname });
-
-        // Guarda los datos en Firestore
         await setDoc(doc(db, "users", user.uid), { nickname, email: user.email });
 
         console.log("Usuario registrado con nickname:", nickname);
@@ -34,4 +30,3 @@ async function registerUser(email, password, nickname) {
         return false;
     }
 }
-
