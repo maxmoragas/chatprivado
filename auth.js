8cd66ea1e5ad0c1562588defd42be7ee3c6405d0
maxmoragas <ixamxp@gmail.com>
2025-05-09 15:39:53 -0300

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,6 +1,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-app.js";
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-auth.js";
 
+
 // Configurar Firebase
 const firebaseConfig = {
     apiKey: "AIzaSyCalxt34jrPFP9VJM5yBFA4BRF2U1_XiZw",
