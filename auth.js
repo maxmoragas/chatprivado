6f72e7f9114426fa72b149e1fea0d322aaa091eb
maxmoragas <ixamxp@gmail.com>
2025-05-09 16:25:35 -0300

    Update auth.js

--- auth.js
+++ auth.js
@@ -6,17 +6,6 @@ document.addEventListener("DOMContentLoaded", () => {
 
     console.log("✅ Firebase cargado correctamente:", firebase);
 
-    // Inicializar Firebase
-    const firebaseConfig = {
-        apiKey: "AIzaSyCalxt34jrPFP9VJM5yBFA4BRF2U1_XiZw",
-        authDomain: "michatprivado-f704a.firebaseapp.com",
-        projectId: "michatprivado-f704a",
-        storageBucket: "michatprivado-f704a.appspot.com",
-        messagingSenderId: "187774286181",
-        appId: "1:187774286181:web:95fc9391a64d3d244e498c"
-    };
-
-    firebase.initializeApp(firebaseConfig);
     const auth = firebase.auth();
 
     // Registro de usuario
