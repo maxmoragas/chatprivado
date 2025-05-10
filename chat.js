71b2a78fad728454015b2585084dc8eacd0141bd
maxmoragas <ixamxp@gmail.com>
2025-05-10 12:51:16 -0300

    Update chat.js

--- chat.js
+++ chat.js
@@ -1,12 +1,10 @@
-console.log("🔍 chat.js se está ejecutando...");
-
 function esperarFirebase() {
     if (window.firebase && window.firebase.database) {
         console.log("✅ Firebase detectado en chat.js:", window.firebase);
         iniciarFirebase();
     } else {
-        console.error("🚨 Firebase aún no está disponible en chat.js. Intentando nuevamente...");
-        setTimeout(esperarFirebase, 1000);
+        console.error("🚨 Firebase aún no está disponible en chat.js. Intentando nuevamente en 3 segundos...");
+        setTimeout(esperarFirebase, 3000);
     }
 }
 
